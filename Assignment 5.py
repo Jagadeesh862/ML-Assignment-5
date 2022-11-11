5745451f744a8f3a16bf90d2c9003bf67166b5b8
Jagadeesh862 <112652802+Jagadeesh862@users.noreply.github.com>
2022-11-11 06:30:20 -0600

    Add files via upload

---
+++ Assignment 5.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# importing required libraries for assignment 5 here
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn import preprocessing, metrics
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+sns.set(style="white", color_codes=True)
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# In[4]:
+
+
+dataset_CC = pd.read_csv("/Users/jagadeeshreddy/Desktop/datasets/CC.csv")
+dataset_CC.info()
+
+
+# In[5]:
+
+
+dataset_CC.head()
+
+
+# In[6]:
+
+
+dataset_CC.isnull().any()
+
+
+# In[7]:
+
+
+dataset_CC.fillna(dataset_CC.mean(), inplace=True)
+dataset_CC.isnull().any()
+
+
+# In[8]:
+
+
+x = dataset_CC.iloc[:,1:-1]
+y = dataset_CC.iloc[:,-1]
+print(x.shape,y.shape)
+
+
+# In[9]:
+
+
+#1.a Apply PCA on CC Dataset
+pca = PCA(3)
+x_pca = pca.fit_transform(x)
+principalDf = pd.DataFrame(data = x_pca, columns = ['principal component 1', 'principal component 2', 'principal component 3'])
+finalDf = pd.concat([principalDf, dataset_CC.iloc[:,-1]], axis = 1)
+finalDf.head()
+
+
+# In[10]:
+
+
+#1.b Apply K Means on PCA Result
+X = finalDf.iloc[:,0:-1]
+y = finalDf.iloc[:,-1]
+
+
+# In[11]:
+
+
+nclusters = 3 # this is the k in kmeans
+km = KMeans(n_clusters=nclusters)
+km.fit(X)
+
+# predict the cluster for each data point
+y_cluster_kmeans = km.predict(X)
+
+
+# Summary of the predictions made by the classifier
+print(classification_report(y, y_cluster_kmeans, zero_division=1))
+print(confusion_matrix(y, y_cluster_kmeans))
+
+
+train_accuracy = accuracy_score(y, y_cluster_kmeans)
+print("\nAccuracy for our Training dataset with PCA:", train_accuracy)
+
+
+#Calculate sihouette Score
+score = metrics.silhouette_score(X, y_cluster_kmeans)
+print("Sihouette Score: ",score) 
+
+"""
+Sihouette Score- ranges from −1 to +1 , a high value indicates that the object is well matched to its own cluster and poorly matched to neighboring clusters.
+"""
+
+
+# In[12]:
+
+
+#1.c Scaling +PCA + KMeans
+x = dataset_CC.iloc[:,1:-1]
+y = dataset_CC.iloc[:,-1]
+print(x.shape,y.shape)
+
+
+# In[13]:
+
+
+#Scaling
+scaler = StandardScaler()
+scaler.fit(x)
+X_scaled_array = scaler.transform(x)
+#PCA
+pca = PCA(3)
+x_pca = pca.fit_transform(X_scaled_array)
+principalDf = pd.DataFrame(data = x_pca, columns = ['principal component 1', 'principal component 2','principal component 3'])
+finalDf = pd.concat([principalDf, dataset_CC.iloc[:,-1]], axis = 1)
+finalDf.head()
+
+
+# In[15]:
+
+
+X = finalDf.iloc[:,0:-1]
+y = finalDf["TENURE"]
+print(X.shape,y.shape)
+
+
+# In[17]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.34,random_state=0)
+nclusters = 3 
+# this is the k in kmeans
+km = KMeans(n_clusters=nclusters)
+km.fit(X_train,y_train)
+
+
+# predict the cluster for each training data point
+y_clus_train = km.predict(X_train)
+
+# Summary of the predictions made by the classifier
+print(classification_report(y_train, y_clus_train, zero_division=1))
+print(confusion_matrix(y_train, y_clus_train))
+
+train_accuracy = accuracy_score(y_train, y_clus_train)
+print("Accuracy for our Training dataset with PCA:", train_accuracy)
+
+#Calculate sihouette Score
+score = metrics.silhouette_score(X_train, y_clus_train)
+print("Sihouette Score: ",score) 
+
+"""
+Sihouette Score- ranges from −1 to +1 , a high value indicates that the object is well matched to its own cluster and poorly matched to neighboring clusters.
+"""
+
+
+# In[18]:
+
+
+# predict the cluster for each testing data point
+y_clus_test = km.predict(X_test)
+
+# Summary of the predictions made by the classifier
+print(classification_report(y_test, y_clus_test, zero_division=1))
+print(confusion_matrix(y_test, y_clus_test))
+
+train_accuracy = accuracy_score(y_test, y_clus_test)
+print("\nAccuracy for our Training dataset with PCA:", train_accuracy)
+
+#Calculate sihouette Score
+score = metrics.silhouette_score(X_test, y_clus_test)
+print("Sihouette Score: ",score) 
+
+"""
+Sihouette Score- ranges from −1 to +1 , a high value indicates that the object is well matched to its own cluster and poorly matched to neighboring clusters.
+"""
+
+
+# In[19]:
+
+
+# Use pd_speech_features.csv
+# a. Perform Scaling
+# b. Apply PCA (k=3)
+# c. Use SVM to report performance
+dataset_pd = pd.read_csv("/Users/jagadeeshreddy/Desktop/datasets/pd_speech_features.csv")
+dataset_pd.info()
+
+
+# In[21]:
+
+
+dataset_pd.isnull().any()
+
+
+# In[22]:
+
+
+X = dataset_pd.drop('class',axis=1).values
+y = dataset_pd['class'].values
+
+
+# In[23]:
+
+
+#Scaling Data
+scaler = StandardScaler()
+X_Scale = scaler.fit_transform(X)
+
+
+# In[24]:
+
+
+# Apply PCA with k =3
+pca3 = PCA(n_components=3)
+principalComponents = pca3.fit_transform(X_Scale)
+
+principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2','Principal Component 3'])
+
+finalDf = pd.concat([principalDf, dataset_pd[['class']]], axis = 1)
+finalDf.head()
+
+
+# In[25]:
+
+
+X = finalDf.drop('class',axis=1).values
+y = finalDf['class'].values
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.34,random_state=0)
+
+
+# In[26]:
+
+
+#2.c Support Vector Machine's 
+
+from sklearn.svm import SVC
+
+svmClassifier = SVC()
+svmClassifier.fit(X_train, y_train)
+
+y_pred = svmClassifier.predict(X_test)
+
+# Summary of the predictions made by the classifier
+print(classification_report(y_test, y_pred, zero_division=1))
+print(confusion_matrix(y_test, y_pred))
+# Accuracy score
+glass_acc_svc = accuracy_score(y_pred,y_test)
+print('accuracy is',glass_acc_svc )
+
+#Calculate sihouette Score
+score = metrics.silhouette_score(X_test, y_pred)
+print("Sihouette Score: ",score)
+
+
+# In[27]:
+
+
+#3.Apply Linear Discriminant Analysis (LDA) on Iris.csv dataset to reduce dimensionality of data to k=2. 
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+dataset_iris = pd.read_csv("/Users/jagadeeshreddy/Desktop/datasets/Iris.csv")
+dataset_iris.info()
+
+
+# In[28]:
+
+
+dataset_iris.isnull().any()
+
+
+# In[29]:
+
+
+x = dataset_iris.iloc[:,1:-1]
+y = dataset_iris.iloc[:,-1]
+print(x.shape,y.shape)
+
+
+# In[30]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+
+
+# In[31]:
+
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+
+# In[32]:
+
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA(n_components=2)
+X_train = lda.fit_transform(X_train, y_train)
+X_test = lda.transform(X_test)
+print(X_train.shape,X_test.shape)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
